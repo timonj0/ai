fd94744550764c03e6f0950e02d6ff97b191691b
TJ001 <tj5036a@gmail.com>
2022-12-27 11:43:27 +0100

    Patch game.py v0.02

--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 """
 Game Hi-Low
-Version: 0.01
+Version: 0.02
 """
 
 from random import randint, choice
@@ -16,21 +16,34 @@ class card:
         self.value = value
         self.revealed = revealed
 
+    def reveal(self):
+        self.revealed = True
+
+    def as_list(self):
+        if self.revealed:
+            return [self.color, self.value]
+        else:
+            return ["##", "##"]
+
 
 class player:
     card_grid = []
+    points = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, points):
+        self.points = points
 
     def print_card_grid(self):
         for row in self.card_grid:
             for c in row:
-                print([c.color, c.value], end='   ')
+                print(c.as_list(), end='   ')
             print("\n")
 
 
 class game:
+    """
+    Game Variables
+    """
     players = []
     colors = ["red", "orange", "green", "lime", "yellow", "blue", "cyan", "pink"]
     values = list(range(-1, 12, 1))
@@ -40,23 +53,72 @@ class game:
     open_stack_card = ""  # Top card from the revealed stack
     closed_stack_card = ""  # Top card from the unrevealed stack
 
+    game_on = True
+    round_on = True
+
+    """
+    General Functions
+    """
+
     def __init__(self, no_players=4):
         self.start_game(no_players)
 
     def random_card(self, revealed=True):
+        """Return a random card"""
         return card(choice(self.colors), choice(self.values), revealed)
 
-    def start_game(self, no_players):
-        for i in range(no_players):
-            current_player = player()
+    """
+    Game Functions
+    """
 
-            # Give Cards to players
+    def start_game(self, no_players):
+        """Initate Players and points"""
+        for i in range(no_players):
+            self.players.append(player(0))
+
+        self.gameloop()  # Start game
+
+    def check_game_on(self):
+        """Check if game is over"""
+        for p in self.players:
+            if p.points >= 100:
+                return True
+        return False
+
+    def gameloop(self):
+        """Gameloop"""
+        while self.game_on:
+            self.start_round()
+            self. game_on = self.check_game_on()
+
+    """
+    Round Functions
+    """
+
+    def start_round(self):
+        """Initiate cards and players at the start of a round"""
+        for current_player in self.players:  # Initiate each players cardgrid with random cards
             for h in range(self.card_grid_height):
                 current_row = []
                 for w in range(self.card_grid_width):
-                    current_row.append(self.__random_card__())
+                    current_row.append(self.random_card())
                 current_player.card_grid.append(current_row)
-            self.players.append(current_player)
+
+        self.roundloop()
+
+    def check_round_on(self):
+        """Check if round is over"""
+        pass
+
+    def turn(self, player: player):
+        pass
+
+    def roundloop(self):
+        """Roundloop"""
+        while self.round_on:
+            for current_player in self.players:
+                self.closed_stack_card = self.random_card(False)
+                self.turn(current_player)
 
 
 mygame = game()
